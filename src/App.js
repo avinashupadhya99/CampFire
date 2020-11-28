61d05c3dca013cdd1ed436df28324cae844648a1
unknown <avinashupadhya99@gmail.com>
2020-11-28 12:01:54 +0530

    Added authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
+import firebase, { auth } from './firebase.js';
+import 'firebase/firestore';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+import SignIn from './components/Authentication/SignIn';
+
+const firestore = firebase.firestore();
+
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       Camp Fire
+      {user ? <LoggedIn /> : <SignIn />}
     </div>
   );
+}
+
+function LoggedIn() {
+  return(
+    <>
+      Logged In
+    </>
+  )
 }
 
 export default App;
